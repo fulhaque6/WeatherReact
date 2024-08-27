804671c0a070555976b08770884aadbaaa22e1fa
fulhaque6 <141068202+fulhaque6@users.noreply.github.com>
2024-08-27 17:28:30 +0500

    location changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,7 +58,10 @@ function Search(props) {
               <span className="input-group-text bg-transparent border-0">
                 <i className="bi bi-geo-alt" style={{ color: "#0288d1" }}>
                   <i className="fa-solid fa-location-dot"
-                  onClick={props.getCurrentLocation}
+                  onClick={()=>{
+                    setInputValue("");
+                    props.getCurrentLocation();
+                  }}
                   ></i>
                 </i>
               </span>
