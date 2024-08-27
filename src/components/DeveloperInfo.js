59229d570f5be32f9064a426eea09af3517a071b
fulhaque6 <141068202+fulhaque6@users.noreply.github.com>
2024-08-27 21:13:02 +0500

    Adding dark mode

--- src/components/DeveloperInfo.js
+++ src/components/DeveloperInfo.js
@@ -1,14 +1,34 @@
+import React, { useState } from "react";
+
 function DeveloperInfo() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
   return (
-    <div className="github-link-container">
-      <a
-        href="https://github.com/fulhaque6/WeatherReact"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="github-link"
-      >
-        <i className="fa-brands fa-github"></i>
-      </a>
+    <div
+      className={`developer-info-container ${
+        isDarkMode ? "dark-mode" : "light-mode"
+      }`}
+    >
+      <div className="github-link-container">
+        <a
+          href="https://github.com/fulhaque6/WeatherReact"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-link"
+        >
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </div>
+      <div className="theme-toggle-container">
+        <label className="theme-toggle-switch">
+          <input type="checkbox" checked={isDarkMode} onChange={toggleTheme} />
+          <span className="slider"></span>
+        </label>
+      </div>
     </div>
   );
 }
