fb1cc4b3a535b1e1e7608b1fa90f2b9dbfda02ae
fulhaque6 <141068202+fulhaque6@users.noreply.github.com>
2024-09-07 20:49:27 +0500

    remove commented code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import "./App.css";
 import React, { useState } from "react";
 import WeatherData from "./components/WeatherData";
 import Search from "./components/Search";
-import Photo from "./components/Photo";
-import Cities from "./utilities/Cities";
 import DeveloperInfo from "./components/DeveloperInfo";
 
 function App() {
   const apiKey = "bcab6e6950bd669881812038ba52d878&units";
-  const apiKeyPhotos = "Yjwb6QtX8g8kasCa40i4cqGP5H0kVEkcRH-mDS7Irf4";
   let [getCityName, setCityName] = useState("");
   let [getWeatherData, setWeatherData] = useState({
     temperature: "",
@@ -20,21 +17,12 @@ function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isLoading, setIsLoading] = useState(false); // State for loader
   const [isWeatherActiveOnce,setWeatherActiveOnce] = useState(false);
-
-  const getWeatherApi = (city) => {
-    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  };
-
   const getGeolocationApi = (lat, long) => {
     return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
   };
 
   const getCitiesApi = (query) => {
     return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${apiKey}`;
-  };
-
-  const getCityPhotoLink = (city) => {
-    return `https://api.unsplash.com/search/photos?query=${city}&client_id=${apiKeyPhotos}`;
   };
 
   const getCurrentLocation = () => {
@@ -102,7 +90,6 @@ function App() {
       {isWeatherActiveOnce === true && (
         <>
           <WeatherData getCity={getCityName} getWeatherData={getWeatherData} />
-          {/* <Photo cityPhoto={getCityPhotoLink} getCity={getCityName} /> */}
         </>
       )}
     </div>
