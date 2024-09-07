1fd0bea8b9d56462b80d9db59087058de5cce498
fulhaque6 <141068202+fulhaque6@users.noreply.github.com>
2024-09-07 22:41:18 +0500

    refactor the code use async and await fot Api calls and used try catch for better exceptional handling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,16 @@
 function Item(props) {
+  const handleClick = () => {
+    props.citiesList([]); // Clear the list of cities
+    props.onClick(props.city); // Pass the clicked city name to the parent component
+    props.showWeather(props.city.lat, props.city.lon); // Show the weather for the clicked city
+  };
+
   return (
     <li
-      className={`list-group-item d-flex align-items-center border-0 rounded mb-2`}
-      onClick={() => {
-        props.citiesList([]);
-        props.onClick(props.city.city);
-        props.showWeather(props.city.lat,props.city.lon);
-      }}
+      className="list-group-item d-flex align-items-center border-0 rounded mb-2"
+      onClick={handleClick}
     >
-      {props.city.city}
+      {props.city}
     </li>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,93 +1,132 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import debounce from "lodash.debounce";
 import Item from "./Item";
 
 function Search(props) {
-  let [getCitiesList, setCitiesList] = useState([]);
-  let [inputValue, setInputValue] = useState("");
+  const [citiesList, setCitiesList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const apiKey = "bcab6e6950bd669881812038ba52d878&units";
 
-  let getCitiesBySearch = async (val) => {
-    let citiesList = [];
-    if (val !== "" && /^[a-zA-Z\s]+$/.test(val)) {
-      const response = await fetch(props.citiesApi(val));
-      const data = await response.json();
-      data.forEach((element) => {
-        citiesList.push({
-          city: element.name,
-          lat: element.lat,
-          lon: element.lon,
-        });
+  const getGeolocationApi = (lat, long) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+
+  const getCitiesApi = (query) => `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${apiKey}`;
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
+
+  const successCallback = (position) => {
+    const { latitude, longitude } = position.coords;
+    getCurrentCity(latitude, longitude);
+  };
+
+  const errorCallback = (error) => {
+    console.error("Error retrieving location:", error);
+  };
+
+  const getCurrentCity = async (latitude, longitude) => {
+    try {
+      props.setIsLoading(true);
+      const response = await fetch(getGeolocationApi(latitude, longitude));
+      if (!response.ok) {
+        throw new Error(`Geolocation API error: ${response.status}`);
+      }
+      const cityData = await response.json();
+      props.setCityName(cityData.name);
+      const weatherInCelsius = (cityData.main.temp - 273.15).toFixed(2);
+      props.setWeatherData({
+        temperature: weatherInCelsius,
+        description: cityData.weather[0].description,
+        humidity: cityData.main.humidity,
+        windSpeed: cityData.wind.speed,
+        main: cityData.weather[0].main,
       });
-      setCitiesList(citiesList);
+      if (!props.isWeatherActiveOnce) {
+        props.setWeatherActiveOnce(true);
+      }
+    } catch (error) {
+      console.error("Error fetching weather for location:", error);
+    } finally {
+      props.setIsLoading(false);
+    }
+  };
+
+  const fetchCitiesBySearch = async (query) => {
+    if (query && /^[a-zA-Z\s]+$/.test(query)) {
+      try {
+        const response = await fetch(getCitiesApi(query));
+        const data = await response.json();
+        const cities = data.map(({ name, lat, lon }) => ({ city: name, lat, lon }));
+        setCitiesList(cities);
+      } catch (error) {
+        console.error("Error fetching cities:", error);
+      }
     } else {
       setCitiesList([]);
     }
   };
 
-  const debouncedSearch = useMemo(() => debounce(getCitiesBySearch, 300), []);
+  const debouncedFetchCities = useMemo(() => debounce(fetchCitiesBySearch, 300), []);
 
-  let handleItemClick = (city) => {
+  useEffect(() => {
+    return () => {
+      debouncedFetchCities.cancel();
+    };
+  }, [debouncedFetchCities]);
+
+  const handleItemClick = (city) => {
     setInputValue(city);
   };
 
-  let handleLocationClick = async () => {
-    setCitiesList([]);
-    setInputValue("");
-    await props.getCurrentLocation();
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    props.setCityName("");
+    props.setWeatherData({
+      temperature: "",
+      description: "",
+      humidity: "",
+      windSpeed: "",
+      main: "",
+    });
+    setInputValue(value);
+    debouncedFetchCities(value);
   };
 
   return (
-    <div className={`container`} style={{ marginTop: "6rem" }}>
+    <div className="container" style={{ marginTop: "6rem" }}>
       <div className="row justify-content-center">
         <div className="col-md-8 col-lg-6">
-          <div
-            className="bg-light rounded-pill shadow-sm"
-            style={{ backgroundColor: "#e0f7fa" }}
-          >
+          <div className="bg-light rounded-pill shadow-sm" style={{ backgroundColor: "#e0f7fa" }}>
             <div className="input-group">
               <span className="input-group-text bg-transparent border-0">
-                <i className="bi bi-search">
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                </i>
+                <i className="fa-solid fa-magnifying-glass"></i>
               </span>
               <input
                 value={inputValue}
-                onChange={(event) => {
-                  props.setCityName("");
-                  props.setWeatherData({
-                    temperature: "",
-                    description: "",
-                    humidity: "",
-                    windSpeed: "",
-                    main: "",
-                  });
-                  setInputValue(event.target.value);
-                  debouncedSearch(event.target.value);
-                }}
+                onChange={handleInputChange}
                 type="text"
                 className="form-control border-0 rounded-pill custom-input"
                 placeholder="Search for a city..."
                 aria-label="Search"
               />
               <span className="input-group-text bg-transparent border-0">
-                <i className="bi bi-geo-alt">
-                  <i
-                    className="fa-solid fa-location-dot"
-                    onClick={handleLocationClick}
-                  ></i>
-                </i>
+                <i className="fa-solid fa-location-dot" onClick={handleLocationClick}></i>
               </span>
             </div>
           </div>
           <ul className="list-group mt-3 rounded shadow-sm">
-            {getCitiesList.map((name, i) => (
+            {citiesList.map((cityObj, i) => (
               <Item
                 key={i}
-                city={name}
-                onClick={handleItemClick}
+                city={cityObj.city}
+                onClick={() => handleItemClick(cityObj.city)}
                 citiesList={setCitiesList}
                 setCityName={props.setCityName}
-                showWeather={props.showWeather}
+                showWeather={() => getCurrentCity(cityObj.lat, cityObj.lon)}
               />
             ))}
           </ul>
